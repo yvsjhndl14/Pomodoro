c1be993b2156cee2e0dd801d1e8a02d353948cf3
yvsjhndl14 <yjd.aguda@gmail.com>
2018-05-01 17:38:44 +0800

    first commit

---
+++ test/e2e/specs/test.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'default e2e tests': (browser) => {
+    // open the browser add check that #app is on the page
+    browser.url('http://localhost:8080').waitForElementVisible('#app', 5000);
+
+    browser.expect.element('logo').to.not.be.visible
+    // check the toggle-volume icon is not visible
+    browser.expect.element('.toggle-volume').to.not.be.visible
+    // check that pause button is disabled
+    browser.expect.element('[title=pause]').to.have.attribute('disabled')
+    // check that stop button is disabled
+    browser.expect.element('[title=stop]').to.have.attribute('disabled')
+    // check that start button is not disabled
+    browser.expect.element('[title=start]').to.not.have.attribute('disabled')
+    // click on start button, check that toggle volume
+    // button is visible
+    browser.click('[title=start]').waitForElementVisible('.toggle-volume', 5000)
+    // check that pause button is not disabled
+    browser.expect.element('[title=pause]').to.not.have.attribute('disabled')
+    // check that stop button is not disabled
+    browser.expect.element('[title=stop]').to.not.have.attribute('disabled')
+    // check stop button is disabled
+    browser.expect.element('[title=start]').to.have.attribute('disabled')
+    browser.end()
+
+  },
+  'wait for kitten test': (browser) => {
+    browser.url('http://localhost:8080').waitForElementVisible('#app', 5000)
+    // initially the kitten element is not visible
+    browser.expect.element('.well.kittens').to.not.be.visible
+    // click on the start button and wait for 7s for kittent element to appear
+    browser.click('[title=start]').waitForElementVisible('.well.kittens', 7000)
+    // check that the image contains the src element that matches thecatapi string
+    browser.expect.element('.well.kittens img').to.have.attribute('src').which.matches(/thecatapi/);
+    browser.end()
+  }
+}
